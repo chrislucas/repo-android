6227c5bd1ecbb853e5d41ba08341cad4db63ebcf
wiselabs <christoffer.luccas@gmail.com>
2016-12-13 09:42:58 -0200

    Mais estudos

--- StudyParcelable/app/src/main/java/wiselabs/com/br/myapplication/Noticia.java
+++ StudyParcelable/app/src/main/java/wiselabs/com/br/myapplication/Noticia.java
@@ -39,7 +39,21 @@ public class Noticia implements Parcelable {
     }
 
     /*
+    *   Definicao da classe Parcel:
+    *   Container para uma referencia de um objeto que pode ser enviado atraves da Interface
+    *   IBinder (https://developer.android.com/reference/android/os/IBinder.html)
+    *   Parcel pode conter dados "comprimidos/achatados", tal processo pode ser feito para
+    *   o envio e desfeito quando a classe que deve receber os dados, recebe-los. A interface
+    *   Parcelable pode ser usada para fazer o trabalho de compressão (serializacao) para o envio.
+    *   Parcel tambem pode manter uma referencia de um objeto do tipo IBinder.
     *
+    *   Segundo a documentacao, a classe Parcel não foi construida como um mecanismo de Serializacao.
+    *   Ela junto a interface Parcelable foram desenvolvidas para implementar alta performace em
+    *   "IPC transport"
+    *
+    *
+    *   Um ponto interessante é que, a classe Parcel não foi projetada para ser armazenada em banco
+    *   de dados. Seu proposito é realizar a leitura e a escrita de quaisquer tipos de dados
     *
     * */
     public static final Parcelable.Creator<Noticia> CREATOR = new Parcelable.Creator<Noticia>() {
@@ -78,4 +92,7 @@ public class Noticia implements Parcelable {
 * Explicacao interessate sobre Parcelable
 * http://pt.stackoverflow.com/questions/38492/quando-e-como-implementar-o-parcelable-vs-serializable
 *
+* pesquisar sobre IPC transport
+* http://pt.slideshare.net/jserv/android-ipc-mechanism
+* http://stackoverflow.com/questions/11994001/is-parcelable-worth-to-implement-to-pass-data-between-activities
 * */
