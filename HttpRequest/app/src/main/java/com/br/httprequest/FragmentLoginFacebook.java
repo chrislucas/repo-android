319f29b72bc302553cec3f5d0d0d13e563490be1
wiselabs <christoffer.luccas@gmail.com>
2016-12-26 09:46:01 -0200

    estudo login facebook

--- HttpRequest/app/src/main/java/com/br/httprequest/FragmentLoginFacebook.java
+++ HttpRequest/app/src/main/java/com/br/httprequest/FragmentLoginFacebook.java
@@ -14,17 +14,24 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-
+/*
+*
+* */
 
 public class FragmentLoginFacebook extends FragmentActivity {
     CallbackManager cbManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.fragment_login_facebook);
+
         cbManager = CallbackManager.Factory.create();
         final LoginButton loginButton = (LoginButton) findViewById(R.id.fragment_login_button);
         loginButton.registerCallback(cbManager, new FacebookCallback<LoginResult>() {
--- HttpRequest/app/src/main/java/com/br/httprequest/LoginFacebook.java
+++ HttpRequest/app/src/main/java/com/br/httprequest/LoginFacebook.java
@@ -9,24 +9,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class LoginFacebook extends AppCompatActivity {
-
+    private  CallbackManager cbManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login_facebook);
+
+        cbManager = CallbackManager.Factory.create();
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.activity_login_button);
+        loginButton.registerCallback(cbManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("LOGIN_FACEBOOK_RESULT", loginResult.toString());
+                Toast.makeText(getApplicationContext(), loginResult.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("LOGIN_FACEBOOK_ERROR", error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Intent intent = new Intent(this, FragmentActivity.class);
-        startActivity(intent);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +64,9 @@ public class LoginFacebook extends AppCompatActivity {
         });
     }
 
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        cbManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
