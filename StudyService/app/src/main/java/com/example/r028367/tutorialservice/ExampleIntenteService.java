ff075d9a38fe71a6164c42a6031783f2bff2ecb1
chrislucas <christoffer.luccas@gmail.com>
2017-01-27 19:54:07 -0200

    estudo service

---
+++ StudyService/app/src/main/java/com/example/r028367/tutorialservice/ExampleIntenteService.java
@@ -0,0 +1,20 @@
+package com.example.r028367.tutorialservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by r028367 on 27/01/2017.
+ */
+
+public class ExampleIntenteService extends IntentService {
+
+    public ExampleIntenteService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
