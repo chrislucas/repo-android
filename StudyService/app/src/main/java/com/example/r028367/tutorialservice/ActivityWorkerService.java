7dc60d8d17274574ac07a3267bc19732acc393f2
wiselabs <christoffer.luccas@gmail.com>
2017-01-30 05:45:06 -0200

    estudo service

---
+++ StudyService/app/src/main/java/com/example/r028367/tutorialservice/ActivityWorkerService.java
@@ -0,0 +1,13 @@
+package com.example.r028367.tutorialservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityWorkerService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_service);
+    }
+}
