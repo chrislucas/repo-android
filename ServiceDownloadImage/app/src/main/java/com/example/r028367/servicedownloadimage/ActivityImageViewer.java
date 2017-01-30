c148af0c1a6ebc1d38070c37054f545d4cde2dc5
chrislucas <christoffer.luccas@gmail.com>
2017-01-30 20:09:14 -0200

    estudos sobre service, broadcastreceveier

---
+++ ServiceDownloadImage/app/src/main/java/com/example/r028367/servicedownloadimage/ActivityImageViewer.java
@@ -0,0 +1,13 @@
+package com.example.r028367.servicedownloadimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityImageViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_viewer);
+    }
+}
