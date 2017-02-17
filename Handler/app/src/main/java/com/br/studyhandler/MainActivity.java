f039993ed82f3a6719e1bb053ba3423f80e50dc5
wiselabs <christoffer.luccas@gmail.com>
2017-02-17 05:57:23 -0200

    estudo sobre handler

--- Handler/app/src/main/java/com/br/studyhandler/MainActivity.java
+++ Handler/app/src/main/java/com/br/studyhandler/MainActivity.java
@@ -3,11 +3,15 @@ package com.br.studyhandler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.br.studyhandler.book.gooand.ExamplesUsingHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ExamplesUsingHandler.runTest1();
+        ExamplesUsingHandler.runTest1(this);
     }
 }
