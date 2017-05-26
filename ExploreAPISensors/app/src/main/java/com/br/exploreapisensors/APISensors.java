1b8e6bda42ede40fa8a180afc783f2d860fe2eda
wiselabs <christoffer.luccas@gmail.com>
2017-05-26 05:19:37 -0300

    nerge

---
+++ ExploreAPISensors/app/src/main/java/com/br/exploreapisensors/APISensors.java
@@ -0,0 +1,13 @@
+package com.br.exploreapisensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APISensors extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apisensors);
+    }
+}
